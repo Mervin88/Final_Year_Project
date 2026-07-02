d352b2c08a2acbda4a269f39a6149c6767275ff5
Mervin88 <tanmervin040803@gmail.com>
2026-07-03 03:56:40 +0800

    Configure backend database for cloud deployment & create requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -38,10 +38,10 @@ CORS(app)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key_eventsync_123456')
 
 # DATABASE CONFIG
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'eventsync'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', '')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'eventsync')
 
 mysql = MySQL(app)
 
